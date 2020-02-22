6c9e40aa99de6550a94bf92c4c0140b3d9dbfede
Beatrice <oprea.beatrice@yahoo.com>
2020-02-22 15:02:31 +0200

    maths project

---
+++ src/main/java/org/fastrackit/Maths.java
@@ -0,0 +1,14 @@
+package org.fastrackit;
+
+public class Maths {
+    static int myMethod(int x, int y) {
+        return x + y;
+    }
+    public static void main(String[] args) {
+        System.out.println(myMethod(5, 3));
+
+    }
+}
+
+
+
